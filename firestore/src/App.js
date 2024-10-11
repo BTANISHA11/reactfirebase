2af46fdb231083c112e4ca2701f6fd1a143fa1e8
TANISHA BANSAL <tanishab130@gmail.com>
2024-10-11 23:15:04 +0530

    firestore

--- firestore/src/App.js
+++ firestore/src/App.js
@@ -1,4 +1,5 @@
-import {getFirestore, collection, addDoc} from "firebase/firestore"
+import {getFirestore, collection, addDoc, doc, getDoc,
+   query, where, getDocs, updateDoc} from "firebase/firestore"
 import {app} from "./firebase";
 import './App.css';
 
@@ -15,13 +16,43 @@ function App() {
   }
 
   const makeSubCollection=async()=>{
-    await addDoc (collection (firestore, 'cities/5J3iWzKLsn5QZlx9LHJv/places'
-  ))}
+    await addDoc (collection (firestore, 'cities/5J3iWzKLsn5QZlx9LHJv/places'),{
+name:"This is a place",
+desc:"Awsm Desc",
+date:Date.now(),
+  })
+}
+
+const getDocument = async () =>{
+  const ref= doc(firestore, "cities" , "5J3iWzKLsn5QZlx9LHJv");
+  const snap=await getDoc(ref);
+  console.log(snap.data());
+}
+
+const getDocumentByQuery = async () =>{
+  const collectionRef= collection(firestore, "users");
+  const q=query(collectionRef, where("isFemale", "==", true));
+  const snapshot=await getDocs(q);
+  snapshot.forEach((data)=>
+    console.log( data.data()))
+  }
+
+  const update =async () =>{
+    const docRef= doc(firestore, 'cities', '5J3iWzKLsn5QZlx9LHJv');
+    await updateDoc(docRef, {
+    name:'New Delhi'
+  })
+  }
+
   return (
     <div className="App">
       <h1>Firebase Firestore</h1>
       <button onClick={writeData}>Put Data</button>
-    </div>
+      <button onClick={makeSubCollection}>Put Sub Data</button>
+      <button onClick={getDocument}>Get Document</button>
+      <button onClick={getDocumentByQuery}>Get Document By Query</button>
+      <button onClick={update}>Update</button>
+      </div>
   );
 }
 
